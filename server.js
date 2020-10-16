30341445bcf306235e38e8df97ed1e16e114795d
FelixvL <digitaleservice@gmail.com>
2020-10-16 15:07:31 +0200

    aanroep naar externe methode EN uitlezen van url variabele

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ var uc = require('upper-case');
 http.createServer(function (req, res) {
 	res.setHeader("Access-Control-Allow-Origin", "*");
   	res.writeHead(200, {'Content-Type': 'text/html'});
-  	/*Use our upper-case module to upper case a string:*/
-  	res.write(uc.upperCase("Hello World!"));
+  	res.write(hetWoordOpmaken(req.url));
   	res.end();
 }).listen(8080);
+
+function hetWoordOpmaken(deurl){
+	return "dit is het antwoord"+deurl;
+}
